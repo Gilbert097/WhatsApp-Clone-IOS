5c669f84e74188dad66dc9c33ff2833259550a0d
Gilberto Silva <betosilva.luz@gmail.com>
2023-08-16 20:31:26 -0300

    Adicionando botão de logout no menu de ajustes.

--- WhatsAppClone/UI/Screens/Settings/SettingsViewController.swift
+++ WhatsAppClone/UI/Screens/Settings/SettingsViewController.swift
@@ -8,26 +8,44 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    private lazy var logoutButtonItem: UIBarButtonItem = {
+        UIBarButtonItem(
+            title: "Deslogar",
+            style: .plain,
+            target: self,
+            action: #selector(logoutButtonTapped)
+        )
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .orange
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.parent?.title = "Ajustes"
+        self.parent?.navigationItem.rightBarButtonItem = self.logoutButtonItem
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.parent?.navigationItem.rightBarButtonItem = nil
+    }
+    
+    @objc private func logoutButtonTapped() {
+        LogUtils.printMessage(tag: "SettingsViewController", message: "---> logoutButtonTapped")
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
