971bf2d07a876aae15227bb33e9c2103ef6a18fe
Gilberto Silva <betosilva.luz@gmail.com>
2023-08-23 16:49:28 -0300

    Implementando TableView na tela de contatos.

--- WhatsAppClone/UI/Screens/MainTabBar/Contacts/ContactsViewController.swift
+++ WhatsAppClone/UI/Screens/MainTabBar/Contacts/ContactsViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return view
+    }()
+    
+    let characters = ["Link", "Zelda", "Ganondorf", "Midna"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .yellow
+        setupView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -19,14 +28,40 @@ class ContactsViewController: UIViewController {
         self.parent?.title = "Contatos"
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: - ViewCode
+extension ContactsViewController: ViewCode {
+    
+    func setupViewHierarchy() {
+        self.view.addSubview(tableView)
     }
-    */
+    
+    func setupConstraints() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+        ])
+    }
+    
+    func setupAdditionalConfiguration() {
+        self.view.backgroundColor = .white
+        self.tableView.dataSource = self
+    }
+}
 
+//MARK: - UITableViewDataSource
+extension ContactsViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return characters.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = characters[indexPath.row]
+    return cell
+  }
 }
